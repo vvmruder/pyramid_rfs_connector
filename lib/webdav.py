e5b0a029de7e5a9efd9688a8c3442d913f67565e
Karsten Deininger <karsten.deininger@bl.ch>
2015-08-06 13:56:54 +0200

    Webdav transfer with source and destination arguments.

--- lib/webdav.py
+++ lib/webdav.py
@@ -19,69 +19,73 @@ __author__ = 'Clemens Rudert'
 
 from python_webdav.connection import Connection, Client
 import os
-import base64
-import binascii
-
-
-
-
-
-"""
-WebDavConnection stellt einen vereinfachten Zugriff auf Webdavressourcen zur Verfuegung.
-Die Klasse wird erzeugt indem
-ihr die URL (komlette URL z.B.: https://www.geo.bl.ch/webdav/baugesuchsscan_temp/) uebergeben wird.
-Es ist moeglich Nutzername und Passwort zu uebergeben. Falls dies nicht geschieht, wird eine Anfrage ohne Nutzerdaten
-gestartet.
-
-Standardmaessig nutzt dieser Helper den Pfad CERT_PATH = "/etc/ssl/certs/ca-certificates.crt" um alle verfuegbaren
-Certifikate nutzen zu koennen. Das kann angepasst werden.
-ACHTUNG: Anpassung an httplib2 wegen Problems mit SSL3 Verbindungen. => Siehe Workaround for SSL3.
-
-Standardmaessig nutzt dieser Helper den Pfad LOCAL_PATH = "/var/sig/public/" um heruntergeladene Dateien abzuspeichern.
-Das kann geaendert werden.
-ACHTUNG: Schreibrechte pruefen!
-"""
 
 
 class WebDavConnection():
+    """
+    WebDavConnection stellt einen vereinfachten Zugriff auf Webdavressourcen zur Verfuegung.
+    Die Klasse wird erzeugt indem
+    ihr die URL (komlette URL z.B.: https://www.geo.bl.ch/webdav/baugesuchsscan_temp/) uebergeben wird.
+    Es ist moeglich Nutzername und Passwort zu uebergeben. Falls dies nicht geschieht, wird eine Anfrage ohne Nutzerdaten
+    gestartet.
+
+    Standardmaessig nutzt dieser Helper den Pfad CERT_PATH = "/etc/ssl/certs/ca-certificates.crt" um alle verfuegbaren
+    Certifikate nutzen zu koennen. Das kann angepasst werden.
+    ACHTUNG: Anpassung an httplib2 wegen Problems mit SSL3 Verbindungen. => Siehe Workaround for SSL3.
+
+    Standardmaessig nutzt dieser Helper den Pfad LOCAL_PATH = "/var/sig/public/" um heruntergeladene Dateien abzuspeichern.
+    Das kann geaendert werden.
+    ACHTUNG: Schreibrechte pruefen!
+    """
+
     CERT_PATH = "/etc/ssl/certs/ca-certificates.crt"
-    LOCAL_PATH = "/var/sig/public/"
 
     def __init__(self, url, username='', password=''):
-        self.connection = Connection(
-            {'host': url, 'port': 443, 'username': username, 'password': password, 'realm': '', 'path': ''})
-        self.connection.httpcon.ca_certs = self.CERT_PATH
+        self.__url__ = url
+        self.__username__ = username
+        self.__password__ = password
         self.webdavclient = Client()
-        self.url = url
 
-    def download_file(self, remote_file_name):
+    def __create_connection__(self):
+        connection = Connection({
+            'host': self.__url__,
+            'port': 443,
+            'username': self.__username__,
+            'password': self.__password__,
+            'realm': '',
+            'path': ''
+        })
+        connection.httpcon.ca_certs = self.CERT_PATH
+        return connection
+
+    def download_file(self, src, dst):
         try:
-            self.webdavclient.get_file(self.connection, remote_file_name, self.LOCAL_PATH + remote_file_name)
-            f = open(self.LOCAL_PATH + remote_file_name)
+            client = Client()
+            client.get_file(self.__create_connection__(), src, dst)
+            f = open(dst)
             file_data = f.read()
             if '404 Not Found' in file_data:
-                os.remove(self.LOCAL_PATH + remote_file_name)
+                os.remove(dst)
                 return None
             else:
-                return self.LOCAL_PATH + remote_file_name
+                return dst
         except Exception as e:
             print e
             pass
 
-    def upload_file(self, remote_file_name, local_file_name):
+    def upload_file(self, src, dst):
         try:
-            self.webdavclient.send_file(self.connection, remote_file_name, self.LOCAL_PATH + local_file_name)
-            #return self.url + remote_file_name
+            client = Client()
+            client.send_file(self.__create_connection__(), dst, src)
         except Exception as e:
             print e
             pass
         
-    def send_put(self, remote_file_name, local_file_name):
+    def send_put(self, src, dst):
         try:
-            f = open(self.LOCAL_PATH + local_file_name, 'rb')
+            f = open(src, 'rb')
             data = f.read()
-            self.connection.send_put(self.url + remote_file_name.encode('utf-8'), data)
-            #return self.url + remote_file_name
+            self.__create_connection__().send_put(os.path.join(self.__url__, dst.encode('utf-8')), data)
         except Exception as e:
             print e
             pass
